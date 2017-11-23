a64eddc7433207bc79b230962c953ec7a31dca4b
SeeeRGo <seeergo@email.com>
2017-11-23 18:11:25 +0400

    Hash pwd before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,27 @@
 const {SHA256} = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-var data = {
-	id: 10
-}
+var password = '123abc!'
+bcrypt.genSalt(10, (err, salt) => {
+	bcrypt.hash(password, salt, (err, hash) => {
+		console.log(hash)
+	})
+})
 
-var token = jwt.sign(data, '123abc')
-console.log(token)
-var decoded = jwt.verify(token, '123abc')
-console.log(decoded)
+var  hashedPassword = '$2a$10$GAYBzW3arRvXMqKx6BO6eOjRFXvvKzvzxTZEK9l/6JWqYp7VCv3Te'
+
+bcrypt.compare('jhgkj', hashedPassword, (err, res) => {
+	console.log(res)
+})
+// var data = {
+// 	id: 10
+// }
+
+// var token = jwt.sign(data, '123abc')
+// console.log(token)
+// var decoded = jwt.verify(token, '123abc')
+// console.log(decoded)
 // var message = 'I am user number 3'
 // var hash = SHA256(message).toString()
 
